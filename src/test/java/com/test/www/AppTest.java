6a09a4775e1e38915f3023c5540fcbec1fe1b83d
studentnotes94925 <studentnotes94925@gmail.com>
2022-12-30 19:51:32 -0800

    202212301951 Testing MVN SELENIUM and JENKENS INTEGRATION

--- src/test/java/com/test/www/AppTest.java
+++ src/test/java/com/test/www/AppTest.java
@@ -1,20 +1,46 @@
 package com.test.www;
 
-import static org.junit.Assert.assertTrue;
-
-import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.*;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
 {
+    private WebDriver driver;
+    private static final String BASE_URL = "https://www.yahoo.com";
+
+    @BeforeSuite
+    public void BeforeSuite()
+    {
+        System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver");
+        this.driver = new ChromeDriver();
+    }
+
     /**
      * Rigorous Test :-)
      */
-    @Test
+    @Test(enabled=false)
     public void shouldAnswerWithTrue()
     {
-        assertTrue( true );
+        Assert.assertTrue( true);
+    }
+
+    @Test(description="FIRST SELENIUM TEST", priority=0)
+    public void firstSeleniumTest()
+    {
+        driver.get(this.BASE_URL);
+    }
+
+    @AfterMethod
+    public void afterMethod() {
+        driver.close();
     }
 }
